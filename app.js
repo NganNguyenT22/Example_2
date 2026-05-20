f392e7cdfa1b24ca297e2220f495f410f62b8b7f
NganNguyenT22 <vozcozdart12443@gmail.com>
2026-05-21 01:16:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  const API_URL = "https://script.google.com/macros/s/AKfycbwET0TQ-QrufLnBeX0lf3iJ_t89ASOueqcj3rDf6TEJC1I512xr33jd_fqSAtg-OdGj-g/exec"; // <-- THAY LINK CỦA BẠN VÀO ĐÂY
+  const API_URL = "https://script.google.com/macros/s/AKfycbyQiO07rwgMKK3Tf5JsTtvVl4J-xW34LhVmWO0LiDn-rrrs7IejX13Ij0VhsRX_xr1V/exec"; // <-- THAY LINK CỦA BẠN VÀO ĐÂY
         
         let currentUser = null;
         let dataNhap = [];
